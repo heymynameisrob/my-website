bd4cd9664511625782e0e2548e50809fc49d2aec
Rob Hough <robhough180@gmail.com>
2024-11-08 20:01:27 +0000

    wrapped search params in suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { HomeBio } from "@/components/home-bio";
 import { HomeTabs } from "@/components/home-tabs";
 import { ThemeToggle } from "@/components/theme";
@@ -11,7 +12,9 @@ export default async function HomePage() {
       <ThemeToggle />
       <main className="relative flex flex-col space-y-12 max-w-7xl mx-auto px-5 py-12 md:py-16 lg:py-28">
         <HomeBio />
-        <HomeTabs postsPromise={postPromise} />
+        <Suspense>
+          <HomeTabs postsPromise={postPromise} />
+        </Suspense>
       </main>
     </>
   );
